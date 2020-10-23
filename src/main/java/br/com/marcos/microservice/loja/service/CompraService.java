5ad41c2ee7b747aa4c135a0fe020d8738193a6ec
Marcos Arruda <marco_wr@outlook.com>
2020-10-22 22:05:34 -0300

    configurações do eureka

--- src/main/java/br/com/marcos/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/marcos/microservice/loja/service/CompraService.java
@@ -2,6 +2,7 @@ package br.com.marcos.microservice.loja.service;
 
 import br.com.marcos.microservice.loja.controller.dto.CompraDto;
 import br.com.marcos.microservice.loja.controller.dto.InfoFornecedorDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,12 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class CompraService {
 
+    @Autowired
+    private RestTemplate client;
+
     public void realizaCompra(CompraDto compra) {
 
-        RestTemplate client = new RestTemplate();
-        ResponseEntity<InfoFornecedorDto> exchange = client.exchange("http://localhost:8081/info/" + compra.getEndereco().getEstado(),
+        ResponseEntity<InfoFornecedorDto> exchange = client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
                 HttpMethod.GET, null, InfoFornecedorDto.class);
 
 
